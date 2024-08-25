c7798cda61e5e43506dd0095e6e1b8cac8136110
cyb3ritic <cyb3ritic@gmail.com>
2024-08-25 12:04:18 +0530

    update dimple labs

--- simple/q46.py
+++ simple/q46.py
@@ -1,5 +1,8 @@
 '''     CLASSWORK 
 Write a python program to get 5 subject markd, store them into array, and also get student name. Find total and display each subject marks.
+
+Expected result:
+Hi username
 '''
 
 #python code for classwork
